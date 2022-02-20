199928ea481239291004a9e82917775a42b1ee77
zjzjzjzj <zjzjzjzj1874@gmail.com>
2022-02-20 18:03:21 +0800

    feat:✨($pprof): add pprof in gin
    Signed-off-by: zjzjzjzj <zjzjzjzj1874@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-contrib/pprof"
 	"github.com/zjzjzjzj1874/gorm-study/pkg/database"
 	"github.com/zjzjzjzj1874/gorm-study/pkg/router"
 )
@@ -9,6 +10,9 @@ func main() {
 	defer database.Close() // 关闭数据库连接
 
 	gin := router.InitRouter() // 初始化路由
+
+	pprof.Register(gin) // pprof分析路由开启
+
 	if err := gin.Run(":8000"); err != nil {
 		panic(err)
 	}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/zjzjzjzj1874/gorm-study
 go 1.17
 
 require (
+	github.com/gin-contrib/pprof v1.3.0
 	github.com/gin-gonic/gin v1.7.7
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/jinzhu/gorm v1.9.16
